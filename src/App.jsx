237b52f3fdd469506137ca8992b796ea6f932ccf
Anmol Sah <kumarxxx242424@gmail.com>
2025-01-14 20:45:33 +0530

    add job-related pages and update routing; include grid background in AppLayout

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,11 @@ import "./App.css";
 import AppLayout from "./layout/AppLayout";
 import LandingPage from "./pages/LandingPage";
 import OnBoarding from "./pages/OnBoarding";
+import JobListing from "./pages/JobListing";
+import Job from "./pages/Job";
+import PostJobs from "./pages/PostJobs";
+import SavedJobs from "./pages/SavedJobs";
+import MyJobs from "./pages/MyJobs";
 
 function App() {
   const router = createBrowserRouter([
@@ -16,6 +21,26 @@ function App() {
         {
           path: "/onboarding",
           element: <OnBoarding />,
+        },
+        {
+          path: "/jobs",
+          element: <JobListing />,
+        },
+        {
+          path: "/job/:id",
+          element: <Job />,
+        },
+        {
+          path: "/post-job",
+          element: <PostJobs />,
+        },
+        {
+          path: "/saved-job",
+          element: <SavedJobs />,
+        },
+        {
+          path: "/my-jobs",
+          element: <MyJobs />,
         },
       ],
     },
