40af23400b6969ed7fd08d6df3361a7a3ace0714
Anmol Sah <kumarxxx242424@gmail.com>
2025-01-14 20:28:29 +0530

    add routing with react-router-dom and create AppLayout and LandingPage components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
 import { Button } from "./components/ui/button";
+import AppLayout from "./layout/AppLayout";
+import LandingPage from "./pages/LandingPage";
 
 function App() {
-  return (
-    <div>
-      <Button>Login</Button>
-    </div>
-  );
+  const router = createBrowserRouter([
+    {
+      element: <AppLayout />,
+      children: [
+        {
+          path: "/",
+          element: <LandingPage />,
+        },
+      ],
+    },
+  ]);
+  return <RouterProvider router={router} />;
 }
 
 export default App;
