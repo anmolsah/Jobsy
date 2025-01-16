ee002fec3a7c45d9f63dbbc8763378f523c882db
Anmol Sah <kumarxxx242424@gmail.com>
2025-01-16 20:31:43 +0530

    enhance Header and LandingPage components with improved styling and layout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,18 +11,24 @@ import {
 const Header = () => {
   return (
     <>
-      <nav className="py-4 flex justify-between items-center">
-        <Link>
-          <img src="/jobsy-logo01.svg" alt="Jobsy" className="h-20" />
+      <nav className="py-4 px-6 text-white flex items-center justify-between shadow-lg">
+        <Link href="/" className="-ml-48">
+          <img src="/jobsy-logo01.svg" alt="Jobsy" className="h-[50px]" />
         </Link>
-        <Button variant="outline">Login</Button>
+
+        <Button
+          variant="outline"
+          className="border border-gray-500 text-gray-200 hover:bg-gray-700 hover:text-white transition-colors duration-300 mr-0"
+        >
+          Login
+        </Button>
 
         {/* <SignedOut>
-          <SignInButton />
-        </SignedOut>
-        <SignedIn>
-          <UserButton />
-        </SignedIn> */}
+    //       <SignInButton />
+    //     </SignedOut>
+    //     <SignedIn>
+    //       <UserButton />
+    //     </SignedIn> */}
       </nav>
     </>
   );
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
 
 const LandingPage = () => {
   return (
-    <div>LandingPage</div>
-  )
-}
+    <main className="flex flex-col gap-10 sm:gap-20 py-10 sm:py-20">
+      <section className="text-center">
+        <h1 className="flex  flex-col items-center justify-center gradient-title text-4xl font-extrabold sm:text-6xl lg:text-8xl tracking-tighter py-4">
+          Find Your Dream Job{" "}
+          <span className="flex items-center gap-2 sm:gap-6">
+            with
+            <img
+              src="/jobsy-logo01.svg"
+              alt="Jobsy"
+              className="h-14 sm:h-24 lg:h-32"
+            />
+          </span>
+        </h1>
+      </section>
+      <div>
+        {/**buttons */}
+        {/**carousel */}
+      </div>
 
-export default LandingPage
+      {/**banner */}
+      <section>{/**cards */}</section>
+
+      {/**acoordian */}
+    </main>
+  );
+};
+
+export default LandingPage;
