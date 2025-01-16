7e5caeec42259e6b990b9f8826bf2057edfa3052
Anmol Sah <kumarxxx242424@gmail.com>
2025-01-16 21:22:59 +0530

    enhance Button component and update LandingPage with job action buttons

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,13 +1,15 @@
+import { Button } from "@/components/ui/button";
 import React from "react";
+import { Link } from "react-router-dom";
 
 const LandingPage = () => {
   return (
     <main className="flex flex-col gap-10 sm:gap-20 py-10 sm:py-20">
       <section className="text-center">
-        <h1 className="flex  flex-col items-center justify-center gradient-title text-4xl font-extrabold sm:text-6xl lg:text-8xl tracking-tighter py-4">
+        <h1 className="flex flex-col items-center justify-center gradient-title text-4xl font-extrabold sm:text-6xl lg:text-8xl tracking-tighter py-4">
           Find Your Dream Job{" "}
           <span className="flex items-center gap-2 sm:gap-6">
-            with
+            {/* with */}
             <img
               src="/jobsy-logo01.svg"
               alt="Jobsy"
@@ -15,10 +17,28 @@ const LandingPage = () => {
             />
           </span>
         </h1>
+        <p className="text-transparent bg-gradient-to-r from-blue-500 via-cyan-200 to-white bg-clip-text sm:mt-4 text-xs sm:text-xl">
+          Browse thousands of job opportunities or find the right talent
+        </p>
       </section>
-      <div>
+      <div className="flex gap-6 justify-center">
         {/**buttons */}
-        {/**carousel */}
+        <Link to="/jobs">
+          <Button
+            size="xl"
+            className="bg-[#6DD4F4] text-white py-0 px-12 rounded-md hover:bg-[#4BB8D7] transition duration-300"
+          >
+            Find Jobs
+          </Button>
+        </Link>
+        <Link to="/post-job">
+          <Button
+            size="xl"
+            className="bg-[#6459F0] text-white py-0 px-12 rounded-md hover:bg-[#4E44D3] transition duration-300"
+          >
+            Post a Job
+          </Button>
+        </Link>
       </div>
 
       {/**banner */}
