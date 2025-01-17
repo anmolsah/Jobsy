c4f11aa18c7c3bfcb51848593256b37c3d3ea37b
Anmol Sah <kumarxxx242424@gmail.com>
2025-01-17 20:04:22 +0530

    implement sign-in overlay in Header component with user authentication handling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,33 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link, useSearchParams } from "react-router-dom";
 import { Button } from "./ui/button";
 import {
   SignedIn,
   SignedOut,
+  SignIn,
   SignInButton,
   UserButton,
 } from "@clerk/clerk-react";
+import { BriefcaseBusiness, Heart, PenBox } from "lucide-react";
 
 const Header = () => {
+  const [showSignIn, setShowSignIn] = useState(false);
+
+  const [search, setSearch] = useSearchParams();
+
+  useEffect(() => {
+    if (search.get("sign-in")) {
+      setShowSignIn(true);
+    }
+  }, [search]);
+
+  const handleOverlayClick = (e) => {
+    if (e.target === e.currentTarget) {
+      setShowSignIn(false);
+      setSearch({});
+    }
+  };
+
   return (
     <>
       <nav className="py-4 px-6 text-white flex items-center justify-between shadow-lg">
@@ -16,20 +35,62 @@ const Header = () => {
           <img src="/jobsy-logo01.svg" alt="Jobsy" className="h-[50px]" />
         </Link>
 
-        <Button
-          variant="outline"
-          className="border border-gray-500 text-gray-200 hover:bg-gray-700 hover:text-white transition-colors duration-300 mr-0"
-        >
-          Login
-        </Button>
-
-        {/* <SignedOut>
-    //       <SignInButton />
-    //     </SignedOut>
-    //     <SignedIn>
-    //       <UserButton />
-    //     </SignedIn> */}
+        <div className="flex gap-8">
+          <SignedOut>
+            <Button
+              variant="outline"
+              onClick={() => setShowSignIn(true)}
+              className="border border-gray-500 text-gray-200 hover:bg-gray-700 hover:text-white transition-colors duration-300 mr-0"
+            >
+              Login
+            </Button>
+          </SignedOut>
+          <SignedIn>
+            {
+              <Button
+                // size="xl"
+                className="bg-[#6459F0] text-white py-0 rounded-full hover:bg-[#4E44D3] transition duration-300"
+              >
+                <PenBox size={20} className="mr-2" />
+                Post a Job
+              </Button>
+            }
+            <Link to="/post-job"></Link>
+            <UserButton
+              appearance={{
+                elements: {
+                  avatarBox: "w-10 h-10",
+                },
+              }}
+            >
+              <UserButton.MenuItems>
+                <UserButton.Link
+                  label="My Jobs"
+                  labelIcon={<BriefcaseBusiness size={15} />}
+                  href="/my-jobs"
+                />
+                <UserButton.Link
+                  label="Saved Jobs"
+                  labelIcon={<Heart size={15} />}
+                  href="/saved-job"
+                />
+              </UserButton.MenuItems>
+            </UserButton>
+          </SignedIn>
+        </div>
       </nav>
+
+      {showSignIn && (
+        <div
+          className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-20"
+          onClick={handleOverlayClick}
+        >
+          <SignIn
+            signUpForceRedirectUrl="/onboarding"
+            fallbackRedirectUrl="/onboarding"
+          />
+        </div>
+      )}
     </>
   );
 };
