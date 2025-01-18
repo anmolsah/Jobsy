7aa2ada993a7c7f6f8bdb4cf8e49a28a4afe72c5
Anmol Sah <kumarxxx242424@gmail.com>
2025-01-18 21:05:48 +0530

    add role selection buttons to OnBoarding component

--- src/pages/OnBoarding.jsx
+++ src/pages/OnBoarding.jsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import { useUser } from "@clerk/clerk-react";
 import React from "react";
 import { BarLoader } from "react-spinners";
@@ -10,10 +11,22 @@ const OnBoarding = () => {
   }
   console.log(user);
   return (
-    <div>
+    <div className="flex flex-col items-center justify-center mt-32">
       <h2 className="gradient-title font-extrabold text-7xl sm:text-8xl tracking-tighter px-8">
-        I am a.....
+        I am a...
       </h2>
+      <div className="mt-16 grid grid-cols-2 gap-4 w-full md:px-40">
+        <Button 
+        
+        className="bg-[#6DD4F4] h-36 text-2xl text-white rounded-md hover:bg-[#4BB8D7] transition duration-300"
+        onClick{()=>handleRoleSelection("candidate")}
+        >
+          Candidate
+        </Button>
+        <Button className="bg-[#6459F0] text-2xl h-36 text-white rounded-md hover:bg-[#4E44D3] transition duration-300">
+          Recrutier
+        </Button>
+      </div>
     </div>
   );
 };
