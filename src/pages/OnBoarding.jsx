0dbd5e9e3297cf6ac8050a118de9ba91dbd538e3
Anmol Sah <kumarxxx242424@gmail.com>
2025-01-18 20:54:08 +0530

    add react-spinners for loading indicator in OnBoarding component

--- src/pages/OnBoarding.jsx
+++ src/pages/OnBoarding.jsx
@@ -1,7 +1,21 @@
+import { useUser } from "@clerk/clerk-react";
 import React from "react";
+import { BarLoader } from "react-spinners";
 
 const OnBoarding = () => {
-  return <div>onBoarding</div>;
+  const { user, isLoaded } = useUser();
+
+  if (!isLoaded) {
+    return <BarLoader className="mb-4" width={"100%"} color="#36d7b7" />;
+  }
+  console.log(user);
+  return (
+    <div>
+      <h2 className="gradient-title font-extrabold text-7xl sm:text-8xl tracking-tighter px-8">
+        I am a.....
+      </h2>
+    </div>
+  );
 };
 
 export default OnBoarding;
