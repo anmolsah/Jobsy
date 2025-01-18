94b5ff234f9377f288e12a06f359042990fc7213
Anmol Sah <kumarxxx242424@gmail.com>
2025-01-18 21:11:30 +0530

    implement role selection handling in OnBoarding component

--- src/pages/OnBoarding.jsx
+++ src/pages/OnBoarding.jsx
@@ -1,29 +1,46 @@
 import { Button } from "@/components/ui/button";
 import { useUser } from "@clerk/clerk-react";
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import { BarLoader } from "react-spinners";
 
 const OnBoarding = () => {
   const { user, isLoaded } = useUser();
+  const navigate = useNavigate();
+
+  const handleRoleSelection = async (role) => {
+    await user
+      .update({
+        unsafeMetadata: { role },
+      })
+      .then(() => {
+        navigate(role === "recruiter" ? "/post-job" : "/jobs");
+      })
+      .catch((err) => {
+        console.error("Failed to update user metadata:", err);
+      });
+  };
 
   if (!isLoaded) {
     return <BarLoader className="mb-4" width={"100%"} color="#36d7b7" />;
   }
-  console.log(user);
+
   return (
     <div className="flex flex-col items-center justify-center mt-32">
       <h2 className="gradient-title font-extrabold text-7xl sm:text-8xl tracking-tighter px-8">
         I am a...
       </h2>
       <div className="mt-16 grid grid-cols-2 gap-4 w-full md:px-40">
-        <Button 
-        
-        className="bg-[#6DD4F4] h-36 text-2xl text-white rounded-md hover:bg-[#4BB8D7] transition duration-300"
-        onClick{()=>handleRoleSelection("candidate")}
+        <Button
+          className="bg-[#6DD4F4] h-36 text-2xl text-white rounded-md hover:bg-[#4BB8D7] transition duration-300"
+          onClick={() => handleRoleSelection("candidate")}
         >
           Candidate
         </Button>
-        <Button className="bg-[#6459F0] text-2xl h-36 text-white rounded-md hover:bg-[#4E44D3] transition duration-300">
+        <Button
+          className="bg-[#6459F0] text-2xl h-36 text-white rounded-md hover:bg-[#4E44D3] transition duration-300"
+          onClick={() => handleRoleSelection("recruiter")}
+        >
           Recrutier
         </Button>
       </div>
