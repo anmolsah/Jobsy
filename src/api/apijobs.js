da8453331e631061ddfd47467b35130fc2b4a37d
Anmol Sah <kumarxxx242424@gmail.com>
2025-01-24 20:53:27 +0530

    fix: correct formatting in getJobs function parameters

--- src/api/apijobs.js
+++ src/api/apijobs.js
@@ -1,5 +1,5 @@
 import supabaseClient from "@/utils/supabase";
-export async function getJobs(token, {location,company_id,searchQuery) {
+export async function getJobs(token, {location,company_id,searchQuery}) {
   const supabase = await supabaseClient(token);
 
   let query = supabase.from("jobs").select("*");
